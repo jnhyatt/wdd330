121a6afad1471aa571dce25fd5c4ae88a49997ac
Josh Hyatt <joshua.n.hyatt@gmail.com>
2026-03-17 22:48:21 -0600

    Dynamic product details

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,31 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.dataSource = dataSource;
+    }
+
+    async addToCart() {
+        const product = await this.dataSource.findProductById(this.productId);
+        const cart = getLocalStorage("so-cart") || [];
+        cart.push(product);
+        setLocalStorage("so-cart", cart);
+    }
+
+    async renderProductDetails() {
+        const product = await this.dataSource.findProductById(this.productId);
+        const template = document.getElementById("product-detail-template");
+        const result = template.content.cloneNode(true);
+        result.querySelector("h3").textContent = product.Brand.Name;
+        result.querySelector("h2").textContent = product.NameWithoutBrand;
+        const img = result.querySelector("img");
+        img.src = product.Image;
+        img.alt = product.Name;
+        result.querySelector(".product-card__price").textContent = `$${product.ListPrice}`;
+        result.querySelector(".product__color").textContent = product.Colors[0].ColorName;
+        result.querySelector(".product__description").innerHTML = product.DescriptionHtmlSimple;
+        result.querySelector("#addToCart").dataset.id = product.Id;
+        return result;
+    }
+}
