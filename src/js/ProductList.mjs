5e0fbf25944cbd72087b14d56b8e6ae00c7b5ff5
Josh Hyatt <joshua.n.hyatt@gmail.com>
2026-03-17 23:14:10 -0600

    Dynamic product list

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,24 @@
+export default class ProductList {
+    constructor(category, dataSource) {
+        this.category = category;
+        this.dataSource = dataSource;
+    }
+
+    async renderProductList() {
+        const products = await this.dataSource.getData();
+        console.log(products);
+        const template = document.getElementById("product-card-template");
+        return products.map((product) => {
+            const result = template.content.cloneNode(true);
+            result.querySelector("a").href = `product_pages/?product=${product.Id}`;
+            const img = result.querySelector("img");
+            img.src = product.Image;
+            img.alt = product.Name;
+            result.querySelector(".card__brand").textContent = product.Brand.Name;
+            result.querySelector(".card__name").textContent =
+                product.NameWithoutBrand;
+            result.querySelector(".product-card__price").textContent = `$${product.ListPrice}`;
+            return result;
+        });
+    }
+}
